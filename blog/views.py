db1d61865feb98f4e2c9ecb5c486bf9ec0548cca
chacon09 <jchacon09@hotmail.com>
2017-09-21 21:21:26 -0600

    Cambie el HTML para la pagina

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
+from .forms import PostForm
 # Create your views here.
 def listar_pub(request):
     pub = Publicacion.objects.filter(fecha_creacion__lte = timezone.now()).order_by('fecha_creacion')
     return render(request, 'blog/listar_pub.html', {'pub':pub})
+
+def detalle_publicar(request, pk):
+    pub = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_publicacion.html', {'pub': pub})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicacion = timezone.now()
+            post.save()
+            return redirect('postear', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    pub = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=pub)
+        if form.is_valid():
+            pub = form.save(commit=False)
+            pub.author = request.user
+            pub.save()
+            return redirect('postear', pk=pub.pk)
+    else:
+        form = PostForm(instance=pub)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
